9d2aaa935daf2714531c48667604968712b6bfdd
swgoodman <spencerwgoodman@icloud.com>
2018-11-14 12:08:46 -0800

    Add initial App.js state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      fetchingData: false,
+      siteData: {}
+    }
+  }
+
   render() {
+
+    const {fetchingData} = this.state
     return (
       <div className="App">
         <header className="App-header">
           <h1>Out There</h1>
-          
+            {
+              fetchingData ?
+                <img src={logo} className="App-logo" alt="logo" />
+              :
+                <p>Content is up to date.</p>
+            }
+
         </header>
       </div>
     );
