2f6faae667a3faceb9facdca5ec89eeb347defa5
swgoodman <spencerwgoodman@icloud.com>
2018-11-15 15:02:01 -0800

    Moved temporary data to Ideas containter

--- src/App.js
+++ src/App.js
@@ -10,19 +10,7 @@ class App extends Component {
     super();
 
     this.state = {
-      fetchingData: false,
-      ideas: {
-        0: {
-          id: 12335,
-          header: "50 Cent should start a cologne brand called 50 Scent",
-          body: "A budget cologne from 50 Cent. On sale now for 50 cents."
-        }
-        1: {
-          id: 12336,
-          header: "An R2D2 Spinoff",
-          body: "His outer dialogue is bleeps, but his inner voice is David Attenborough"
-        }
-      }
+      fetchingData: false
     }
   }
 
--- src/containers/ideaContainers.js
+++ src/containers/ideaContainers.js
@@ -6,6 +6,27 @@ import CommentInput from '../components/commentInput.js'
 import ReactionInput from '../components/reactionInput.js'
 
 class IdeaContainers extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      ideas: {
+        0: {
+          id: 12335,
+          header: "50 Cent should start a cologne brand called 50 Scent",
+          body: "A budget cologne from 50 Cent. On sale now for 50 cents."
+        },
+        1: {
+          id: 12336,
+          header: "An R2D2 Spinoff",
+          body: "His outer dialogue is bleeps, but his inner voice is David Attenborough"
+        }
+      }
+    }
+  }
+
+
     render(){
       return (
         <div>
