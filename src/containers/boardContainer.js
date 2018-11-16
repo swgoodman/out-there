3765db00acb16dfb96de0787766259c45af3d3e3
swgoodman <spencerwgoodman@icloud.com>
2018-11-15 16:07:18 -0800

    Reordanized data Board objects in an array

--- src/containers/boardContainer.js
+++ src/containers/boardContainer.js
@@ -9,38 +9,39 @@ class Board extends Component {
     super();
 
     this.state = {
-      0: {
+      boards: [
+      {
         boardId: "board123",
         boardName: "Company Board",
-        ideas: {
-          0: {
+        ideas: [
+          {
             id: 12335,
             header: "Augmented Reality Capabilities",
             body: "What if you could scan a plant and immediate receive information about it?"
           },
-          1: {
+          {
             id: 12336,
             header: "Artificially Intelligent Office Dog",
             body: "No walking or "
           }
-        }
+        ]
       },
-      1: {
+      {
         boardId: "board456",
         boardName: "Personal Board",
-        ideas: {
-          0: {
+        ideas: [
+          {
             id: 12335,
             header: "50 Cent should start a cologne brand called 50 Scent",
             body: "A budget cologne from 50 Cent. On sale now for 50 cents."
           },
-          1: {
+          {
             id: 12336,
             header: "An R2D2 Spinoff",
             body: "His outer dialogue is bleeps, but his inner voice is David Attenborough"
           }
-        }
-      }
+        ]
+      }]
     }
   }
 
