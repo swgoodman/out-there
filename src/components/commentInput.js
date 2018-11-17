f2e6e3df1449bc09b36194f47876805ddaefe77d
swgoodman <spencerwgoodman@icloud.com>
2018-11-17 15:20:48 -0800

    Added Comment input form

--- src/components/commentInput.js
+++ src/components/commentInput.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const CommentInput = () => {
-      return (
+class CommentInput extends Component {
+
+  constructor(props) {
+  super(props);
+
+    this.state = {
+      header: '',
+      body: ''
+    };
+  };
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.addComment(this.state)
+    this.setState({
+      header: '',
+      body: ''
+    });
+  }
+
+  render() {
+    return (
         <div>
-          <p>Comment Input!</p>
+          <form onSubmit={(event) => this.handleOnSubmit(event)} >
+            <input type="text" value={this.state.header} onChange={(event) => this.handleOnChange(event)} />
+            <input type="submit" />
+          </form>
         </div>
       );
+    }
 
 }
 
