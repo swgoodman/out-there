5c9e2a8e69592f169e26d136bcf8cdd56a728546
swgoodman <spencerwgoodman@icloud.com>
2018-11-23 16:47:14 -0800

    Added Clap Reactions functionality

--- src/components/reactions.js
+++ src/components/reactions.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Reactions = () => {
-      return (
+class Reactions extends Component {
+
+  state = {
+    reactionCount: 0
+  }
+
+  increaseCount = e => {
+    e.preventDefault();
+    this.setState({
+      reactionCount: this.state.reactionCount + 1
+    })
+  }
+
+
+  render() {
+    return (
         <div>
-          <p>Reactions!</p>
+          <p>{this.state.reactionCount} claps!</p>
+          <button
+            name="reactionCount"
+            value={this.state.reactionCount}
+            onClick={this.increaseCount}>
+          CLAP!!
+          </ button>
         </div>
       );
+    }
 
 }
 
