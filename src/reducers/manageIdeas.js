16a79f3073a2a77db5f0956e37b82d234114af03
swgoodman <spencerwgoodman@icloud.com>
2018-11-27 15:28:28 -0800

    Setting up Reducer

---
+++ src/reducers/manageIdeas.js
@@ -0,0 +1,37 @@
+import cuid from 'cuid';
+export const cuidFn = cuid;
+
+export default function manageIdeas(state = {
+  ideas: [],
+  comments: [],
+}, action) {
+  switch (action.type) {
+
+    case 'ADD_IDEA':
+
+      const idea = { header: action.header, body: action.body, id: cuidFn() };
+      return {
+        ...state,
+        ideas: [ ...state.ideas, idea]
+      }
+
+    case 'DELETE_IDEA':
+      const ideas = state.ideas.filter(idea => idea.id !== action.id);
+      return { ...state, ideas}
+
+    case 'ADD_COMMENT':
+
+      const comment = { text: action.comment.body, ideaId: action.idea.ideaId, id: cuidFn() };
+      return { ...state,
+        comments: [...state.comments, comment]
+      }
+
+    case 'DELETE_COMMENT':
+      const comments = state.comments.filter(comment => comment.id !== action.id);
+      return {...state, comments }
+
+    default:
+      return state;
+
+  }
+};
