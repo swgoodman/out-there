dce42f071496bdbe334053a6813c1403ab6604db
swgoodman <spencerwgoodman@icloud.com>
2018-11-30 15:43:47 -0800

    Connect User Reducer to Action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import manageIdeas from './manageIdeas.js';
+import manageUsers from './manageUsers.js';
 
-export default combineReducers({
-  manageIdeas
+const rootReducer = combineReducers({
+  ideas: manageIdeas,
+  users: manageUsers
 });
+
+export default rootReducer;
