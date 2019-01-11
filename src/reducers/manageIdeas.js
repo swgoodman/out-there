bb2e7b4ebede9c5eea87a3e00834199d7fd6bb86
swgoodman <spencerwgoodman@icloud.com>
2019-01-11 13:07:47 -0800

    Added Current to store

--- src/reducers/manageIdeas.js
+++ src/reducers/manageIdeas.js
@@ -2,6 +2,7 @@ import cuid from 'cuid';
 export const cuidFn = cuid;
 
 const initialState =  {
+  current: {},
   all: [
     {
       id: '1',
@@ -25,20 +26,7 @@ export default function manageIdeas(state = initialState, action) {
         ...state,
         all: [ ...state.all, idea ]
       }
-    // case 'DELETE_IDEA':
-    //   const all = state.all.filter(idea => idea.id !== action.id);
-    //   return { ...state, all}
-    //
-    // case 'ADD_COMMENT':
-    //
-    //   const comment = { text: action.comment.body, ideaId: action.idea.ideaId, id: cuidFn() };
-    //   return { ...state,
-    //     comments: [...state.comments, comment]
-    //   }
-    //
-    // case 'DELETE_COMMENT':
-    //   const comments = state.comments.filter(comment => comment.id !== action.id);
-    //   return {...state, comments }
+
     default:
       return state;
   }
