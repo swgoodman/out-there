73de98e7037e92281f1389fb7fcdfc347af85e32
swgoodman <spencerwgoodman@icloud.com>
2019-01-14 13:16:59 -0800

    Setting up comment iteration

--- src/components/idea.js
+++ src/components/idea.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Comment from './comment'
+import CommentInput from './commentInput'
 
-const Idea = ({ idea: { id, header, body } }) =>
+const Idea = ({ idea: { id, header, body, comments }, deleteIdea, deleteComment }) =>
   <div>
     <h1>{ header }</h1>
     <p>{ body }</p>
+    <ul>
+      { comments.map(comment => <Comment key={ comment.id } comment = { comment } ideaId={ id } deleteComment={ deleteComment } />)}
+    </ul>
   </div>
 
 export default Idea
