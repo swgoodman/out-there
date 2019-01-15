16cb01203caccafe8abf4be155479014c22bcb25
swgoodman <spencerwgoodman@icloud.com>
2019-01-14 17:36:42 -0800

    User login container finished

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route, Redirect, Link } from 'react-router-dom'
 
+import Login from './containers/Login'
+import Signup from './containers/Signup'
 import IdeaList from './containers/IdeaList'
 
 export default (
   <BrowserRouter>
     <Switch id='routes'>
       <Route exact path='/' render={ () => <IdeaList /> } />/>
+      <Route path='/signup' component={ () => loggedIn() ? <Redirect to="/"/> : <Signup/> }/>
+      <Route path='/login' component={ () => loggedIn() ? <Redirect to="/"/> : <Login/> }/>
+      <Route path='/logout' component={ () => logout() }/>
+      <Route path='/ideas' component={ () =>  loggedIn() ? <IdeaList/> : <Redirect to="/login"/> }/>
     </Switch>
   </BrowserRouter>
 )
