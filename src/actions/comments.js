63e15fd8fdb7c93805ebe1ea66f19fff167c0110
swgoodman <spencerwgoodman@icloud.com>
2019-01-16 11:23:45 -0800

    Fetch Comments Action done

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,5 +1,25 @@
 const baseUrl = 'http://localhost:3000/api/v1'
 
+export const fetchComments = idea => {
+  let data = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+
+  return dispatch => {
+    fetch(`${ baseUrl }/ideas/${ idea.id }/comments`, data)
+      .then(response => response.json())
+      .then(ideas => dispatch({
+        type: 'FETCH_COMMENTS',
+        payload: comments
+      }))
+      .catch(err => err)
+  }
+}
+
 export const createComment = (comment, ideaId) => {
   let data = {
     method: 'POST',
