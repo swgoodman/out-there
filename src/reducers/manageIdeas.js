8d652a81bd187df5b2866071d62380bbdbf98c21
swgoodman <spencerwgoodman@icloud.com>
2019-01-23 12:31:26 -0800

    Removed dummy data :

--- src/reducers/manageIdeas.js
+++ src/reducers/manageIdeas.js
@@ -3,44 +3,7 @@ export const cuidFn = cuid;
 
 const initialState =  {
   current: {},
-  all: [
-    {
-      id: '1',
-      header: "Russian Manchurian Candidate",
-      body: "this is a great idea.",
-      comments: [
-        { id: 1, body: "This is a great idea!"},
-        { id: 2, body: "I agree!"}
-      ]
-    },
-    {
-      id: '2',
-      header: "Beaux for President",
-      body: "2020",
-      comments: [
-        { id: 1, body: "This is a great idea!"},
-        { id: 2, body: "I agree!"}
-      ]
-    },
-    {
-      id: '3',
-      header: "Young Thug is a god?",
-      body: "Read a book.",
-      comments: [
-        { id: 1, body: "This is a great idea!"},
-        { id: 2, body: "I agree!"}
-      ]
-    },
-    {
-      id: '4',
-      header: "Lightning Chicken Nuggets",
-      body: "Sounds like flying cars",
-      comments: [
-        { id: 1, body: "This is a great idea!"},
-        { id: 2, body: "I agree!"}
-      ]
-    }
-  ]
+  all: []
 }
 
 export default function todosReducer(state = initialState, action) {
