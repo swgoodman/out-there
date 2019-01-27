08720194127ac9b6161411d45dead8686f1f4b92
swgoodman <spencerwgoodman@icloud.com>
2019-01-27 15:33:16 -0800

    Wiring up Board actions

--- src/reducers/manageBoards.js
+++ src/reducers/manageBoards.js
@@ -1,30 +1,16 @@
-import cuid from 'cuid';
-export const cuidFn = cuid;
-
 const initialState =  {
   current: {},
   all: []
 }
 
-export default function manageBoards(state = initialState, action) {
+export default function manageBoards (state = initialState, action) {
   switch(action.type) {
     case 'FETCH_BOARDS':
       return { ...state, all: action.payload }
-    case 'CREATE_IDEA':
+    case 'CREATE_BOARD':
       return { ...state, all: [...state.all, action.payload] }
-    case 'DELETE_IDEA':
+    case 'DELETE_BOARD':
       return { ...state, all: state.all.filter(idea => idea.id !== action.payload.id) }
-    case 'CREATE_COMMENT' :
-      let all = [ ...state.all ]
-      let idx = all.findIndex(idea => idea.id === action.payload.idea_id)
-      all[idx].comments.push(action.payload)
-      return { ...state, all }
-    case 'DELETE_COMMENT':
-      all = [ ...state.all ]
-      idx = all.findIndex(idea => idea.id === action.payload.idea_id)
-      all[idx].comments = all[idx].comments.filter(comment => comment.id !== action.payload.id)
-
-      return { ...state, all }
     default: return state
   }
 }
