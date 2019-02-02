0e915aacff3d9ba0d9441aab421e9c63d255d9d4
swgoodman <spencerwgoodman@icloud.com>
2019-02-01 16:34:56 -0800

    Working on styling

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledList = styled.li`
+  color: black;
+  font-size: 14px;
+  font-style: italic;
+  display: inline-block;
+  text-decoration: none;
+  text-align: center;
+`
+
+const StyledCommentButton = styled.button`
+  border: none;
+  color: black;
+  margin: 10px;
+  font-size: 10px;
+  font-style: italic;
+  display: inline-block;
+  text-decoration: none;
+  text-align: center;
+`
 
 const Comment = ({ comment: { id, body }, ideaId, deleteComment}) => {
       return (
         <div>
-          <li>
+          <StyledList>
             { body }
-            <button onClick={ () => deleteComment(id, ideaId) }>Delete</button>
-          </li>
+            <StyledCommentButton onClick={ () => deleteComment(id, ideaId) }>X</StyledCommentButton>
+          </StyledList>
         </div>
       );
 
