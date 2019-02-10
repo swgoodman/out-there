cf6ea1d527bd8c63f2dd6e558197ce3b6be381a8
swgoodman <spencerwgoodman@icloud.com>
2019-02-10 13:56:54 -0800

    Styling input

--- src/components/ideaInput.js
+++ src/components/ideaInput.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { createIdea } from '../actions/ideas'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles, createStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+const styles = theme => ({
+  divider: {
+    margin: 10,
+  },
+  text: {
+    width: 200,
+    height: 30,
+    margin: 5,
+  },
+})
 
 class IdeaInput extends Component {
 
@@ -34,24 +50,29 @@ class IdeaInput extends Component {
 
     render() {
       const { header, body } = this.state
+      const { classes } = this.props
 
       return (
         <div>
             <form onSubmit={this.handleSubmit}>
-              <input
+              <TextField
+                className={classes.text}
+                variant="outlined"
                 type="text"
                 name="header"
                 placeholder="idea"
                 value= { header }
                 onChange={this.handleChange} />
-              <input
+              <TextField
+                className={classes.text}
+                variant="outlined"
                 type="text"
                 name="body"
                 placeholder="details"
                 value={ body }
                 onChange={this.handleChange}
                  />
-              <button type="submit">Submit</button>
+              <Button className={classes.button} color="secondary" type="submit">Submit</Button>
             </form>
         </div>
       );
@@ -59,8 +80,12 @@ class IdeaInput extends Component {
 
 }
 
+IdeaInput.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
 const mapDispatchToProps = dispatch => bindActionCreators({
   createIdea
 }, dispatch)
 
-export default connect(null, mapDispatchToProps)(IdeaInput)
+export default withStyles(styles) (connect(null, mapDispatchToProps)(IdeaInput))
--- src/containers/IdeaContainer.js
+++ src/containers/IdeaContainer.js
@@ -8,15 +8,43 @@ import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import Idea from '../components/Idea.js'
 import IdeaInput from '../components/IdeaInput.js'
+import { withStyles, createStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import Divider from '@material-ui/core/Divider'
+
+const styles = theme => ({
+  main: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 1,
+    display: 'flex',
+    flexDirection: 'column',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  divider: {
+    margin: 10,
+  },
+})
 
 class IdeaContainer extends Component {
 
   render() {
-    const { ideas, board } = this.props
+    const { ideas, board, classes } = this.props
     return (
       <div>
-        <Paper>
+        <Paper className={classes.paper}>
           <Typography variant="h4">{board.name}</Typography>
+          <Divider className={classes.divider}/>
           <IdeaInput current={ board }/>
         </Paper>
 
@@ -36,10 +64,14 @@ class IdeaContainer extends Component {
   }
 }
 
+IdeaInput.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
 const mapDispatchToProps = dispatch => bindActionCreators({
   fetchIdeas,
   deleteIdea,
   deleteComment
 }, dispatch)
 
-export default connect(null, mapDispatchToProps)(IdeaContainer)
+export default withStyles(styles) (connect(null, mapDispatchToProps)(IdeaContainer))
